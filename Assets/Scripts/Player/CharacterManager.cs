4f46fde3228ec009b25c7f86fd2c7d20d550aa9a
kjm0417 <ekdh41779@gmail.com>
2024-10-28 23:23:55 +0900

    플레이어 이동

---
+++ Assets/Scripts/Player/CharacterManager.cs
@@ -0,0 +1,52 @@
+    using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class CharacterManager : MonoBehaviour
+{
+    private static CharacterManager instance;
+
+    public static CharacterManager Instance
+    {   
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameObject("CharacterManager").AddComponent<CharacterManager>(); //캐릭터매니저가 없으면 새로운 게임 오브젝트에 추가해서 만듬
+            }
+            return instance; 
+        }
+
+    }
+    private Player player;
+
+    public Player Player
+    {
+        get { return player; }
+        set { player = value; }
+    }
+
+
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
